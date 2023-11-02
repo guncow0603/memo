d47cd29e4c162a0c0c52301069af21d822026154
guncow0603 <guncow0603@gmail.com>
2023-11-02 11:20:25 +0900

    Create, Read 구현

--- src/main/java/com/sparta/memo/dto/MemoRequestDto.java
+++ src/main/java/com/sparta/memo/dto/MemoRequestDto.java
@@ -8,5 +8,8 @@ public class MemoRequestDto {
     private String username;
     private String contents;
 
-
+    public MemoRequestDto(Memo memo) {
+        this.username = memo.getUsername();
+        this.contents = memo.getContents();
+    }
 }
